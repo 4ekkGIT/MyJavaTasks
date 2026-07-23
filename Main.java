3820438864f45bdd72ec19bbb83c311638101748
4ekkGIT <alikhan.seidakbarov@gmail.com>
2026-07-23 21:27:35 +0500

    done

--- Main.java
+++ Main.java
@@ -1,6 +1,18 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // Print the greeting below.
-        System.out.println("Hello, Java!");
+        Scanner sc = new Scanner(System.in);
+        int[] nums = Arrays.stream(sc.nextLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+        int result = Arrays.stream(nums)
+                .filter(x -> x % 2 == 0)
+                .map(x -> x * x)
+                .sum();
+
+        System.out.println(result);
     }
 }
